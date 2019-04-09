5e6827d239f4cd65e9c0c7aa39bafc84c9d24b73
Aaron Denny <reapor12@hotmail.com>
2019-04-08 22:52:08 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -266,8 +266,8 @@ function render() {
         if (textObjects[i].timer > 0) ctx.fillText(textObjects[i].text, textObjects[i].x, textObjects[i].y);
     }
     
-    ctx.fontStyle = "Bold 32px Calibri"
-    if (side == uSide && turnTimer < 150) ctx.fillText("Your Turn", 1920 / 2, 1080 / 2);
+    ctx.font = "Bold 144px Calibri";
+    if (side == uSide && turnTimer < 500) ctx.fillText("Your Turn", 1920 / 2, 1080 / 2);
 }
 
 function move(fx, fy, tx, ty) {
